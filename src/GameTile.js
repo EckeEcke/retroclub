4c6406c5c0b5cc4fcfacd4c0e20f818a6c2971ec
Christian Eckardt <eckardt.christian@web.de>
2025-01-22 14:40:28 +0100

    add more testdata and rating table

--- src/GameTile.js
+++ src/GameTile.js
@@ -1,16 +1,53 @@
+import React from 'react'
 import './GameTile.css'
 import { gameData } from './TestData'
 
 const GamingTile = () => {
   return (
-    <div class="game-card">
-      <img src={gameData.image.original_url} alt="" />
-      <h2>{gameData.name}</h2>
-      <div class="platforms">
-        {gameData.platforms.map(platform => (
-          <span class="platform-span" key={platform.id}>{platform.name}</span>
-        ))}
-      </div>
+    <div class="tile-container">
+      {gameData.map((game, index) => (
+        <div className="game-card" key={index}>
+          <div class="image-container">
+            <img src={game.image.original_url} alt={game.name} />
+          </div>
+          <h2>{game.name}</h2>
+          <div className="release-year">veröffentlicht {game.expected_release_year || game.original_release_date}</div>
+          <div className="platforms">
+            {game.platforms.map(platform => (
+              <span className="platform-span" key={platform.id}>{platform.name}</span>
+            ))}
+          </div>
+          <div class="ratings">
+          <h2>Bewertungen</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Player</th>
+                <th>Gesamt</th>
+                <th>Thema</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Lena</td>
+                <td>10</td>
+                <td>10</td>
+              </tr>
+              <tr>
+                <td>Rene</td>
+                <td>10</td>
+                <td>10</td>
+              </tr>
+              <tr>
+                <td>Chris</td>
+                <td>10</td>
+                <td>10</td>
+              </tr>
+            </tbody>
+          </table>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
--- src/TestData.js
+++ src/TestData.js
@@ -1,5 +1,5 @@
 export 
-const gameData = {
+const gameData = [{
   aliases: "Phalanx: The Enforce Fighter A-144",
   api_detail_url: "https://www.giantbomb.com/api/game/3030-18519/",
   date_added: "2008-04-01 16:40:59",
@@ -92,4 +92,66 @@ const gameData = {
   ],
   site_detail_url: "https://www.giantbomb.com/phalanx/3030-18519/",
   resource_type: "game"
-  }
+  },
+  {
+    aliases: "Muscle Koushinkyoku Massuru Koushinkyoku",
+    api_detail_url: "https://www.giantbomb.com/api/game/3030-26450/",
+    date_added: "2009-05-15 15:14:24",
+    date_last_updated: "2024-11-07 21:25:02",
+    deck: "A comedic WiiWare game about bodybuilders chasing down protein-nabbing thieves in a single-file line, posing to match the hole in the wall.",    expected_release_day: null,
+    expected_release_month: null,
+    expected_release_quarter: null,
+    expected_release_year: null,
+    guid: "3030-26450",
+    id: 26450,
+    image: {
+    icon_url: "https://www.giantbomb.com/a/uploads/square_avatar/0/26/1204914-muscle_march_announce.jpg",
+    medium_url: "https://www.giantbomb.com/a/uploads/scale_medium/0/26/1204914-muscle_march_announce.jpg",
+    screen_url: "https://www.giantbomb.com/a/uploads/screen_medium/0/26/1204914-muscle_march_announce.jpg",
+    screen_large_url: "https://www.giantbomb.com/a/uploads/screen_kubrick/0/26/1204914-muscle_march_announce.jpg",
+    small_url: "https://www.giantbomb.com/a/uploads/scale_small/0/26/1204914-muscle_march_announce.jpg",
+    super_url: "https://www.giantbomb.com/a/uploads/scale_large/0/26/1204914-muscle_march_announce.jpg",
+    thumb_url: "https://www.giantbomb.com/a/uploads/scale_avatar/0/26/1204914-muscle_march_announce.jpg",
+    tiny_url: "https://www.giantbomb.com/a/uploads/square_mini/0/26/1204914-muscle_march_announce.jpg",
+    original_url: "https://www.giantbomb.com/a/uploads/original/0/26/1204914-muscle_march_announce.jpg",
+    image_tags: "All Images,Concept Art"
+    },
+    image_tags: [
+    {
+    api_detail_url: "https://www.giantbomb.com/api/images/3030-26450/?filter=image_tag:All%20Images",
+    name: "All Images",
+    total: 32
+    },
+    {
+    api_detail_url: "https://www.giantbomb.com/api/images/3030-26450/?filter=image_tag:Concept%20Art",
+    name: "Concept Art",
+    total: 2
+    },
+    {
+    api_detail_url: "https://www.giantbomb.com/api/images/3030-26450/?filter=image_tag:Screenshots",
+    name: "Screenshots",
+    total: 30
+    }
+    ],
+    name: "Muscle March",
+    number_of_user_reviews: 0,
+    original_game_rating: [
+    {
+    api_detail_url: "https://www.giantbomb.com/api/game_rating/3065-29/",
+    id: 29,
+    name: "ESRB: E10+"
+    }
+    ],
+    original_release_date: "2009-05-26",
+    platforms: [
+    {
+    api_detail_url: "https://www.giantbomb.com/api/platform/3045-87/",
+    id: 87,
+    name: "Wii Shop",
+    site_detail_url: "https://www.giantbomb.com/wii-shop/3045-87/",
+    abbreviation: "WSHP"
+    }
+    ],
+    site_detail_url: "https://www.giantbomb.com/muscle-march/3030-26450/",
+    resource_type: "game"
+    }]
