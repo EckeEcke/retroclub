29259f6a5d432c20d5c99f278bb6430d63a5b268
Christian Eckardt <eckardt.christian@web.de>
2025-01-22 16:34:40 +0100

    adding login modal and stylings

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,17 @@
 import './Header.css'
+import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function Header() {
+    const [isOpen, setIsOpen] = useState(false)
+  
+    const openModal = () => {
+      setIsOpen(true)
+    }
+  
+    const closeModal = () => {
+      setIsOpen(false)
+    }
   return (
     <header>
       <div class="container container-header">
@@ -10,9 +20,22 @@ function Header() {
           <h1>Retroclub</h1>
         </div>
         <button>
-          <FontAwesomeIcon icon="fa-solid fa-user" />
+          <FontAwesomeIcon icon="fa-solid fa-user" onClick={openModal} />
         </button>
       </div>
+      {isOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>&times;</span>
+            <h2>Anmelden</h2>
+            <form>
+              <input placeholder="Passwort eingeben" type="text" />
+              <button type="submit">Senden</button>
+            </form>
+            
+          </div>
+        </div>
+      )}
     </header>
   )
 }
