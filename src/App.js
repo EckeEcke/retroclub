ca843e56721e8188dcd0ac111864ade94bd11323
Christian Eckardt <eckardt.christian@web.de>
2025-01-23 11:39:58 +0100

    add test serverless function

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react'
 import Header from './Header.js'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
@@ -6,6 +7,11 @@ import ThemeDropdown from './ThemeDropdown.js'
 library.add(fas)
 
 function App() {
+  useEffect(() => {
+    fetch('/api/hello')
+      .then(response => response.json())
+      .then(data => console.log(data.message))
+  }, [])
   return (
     <div className="App">
       <Header />
---
+++ src/api/hello.js
@@ -0,0 +1,4 @@
+export default function handler(req, res) {
+    res.status(200).json({ message: 'If you can read me, the serverless function works' })
+  }
+  
