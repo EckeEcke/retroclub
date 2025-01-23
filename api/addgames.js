953bf5f1c1c8e8f621bc3d098726e452915bd313
Christian Eckardt <eckardt.christian@web.de>
2025-01-23 18:37:24 +0100

    only add themes and games that dont exist yet

--- api/addgames.js
+++ api/addgames.js
@@ -17,24 +17,43 @@ export default async function handler(req, res) {
 
   try {
     const fetch = (await import('node-fetch')).default
-
-    const results = await Promise.all(ids.map(async (id) => {
-      const response = await fetch(`https://www.giantbomb.com/api/game/${id}/?api_key=${apiKey}&format=json`)
-      const data = await response.json()
-      return data
-    }))
-
     const db = await connectToDatabase()
     const collection = db.collection('themes')
+    const existingTheme = await collection.findOne({ name: theme })
 
-    const document = {
-      name: theme,
-      games: results
+    if (existingTheme) {
+      const newGames = await Promise.all(ids.map(async (id) => {
+        const response = await fetch(`https://www.giantbomb.com/api/game/${id}/?api_key=${apiKey}&format=json`)
+        const data = await response.json()
+        return data
+      }))
+
+      const gamesToAdd = newGames.filter(newGame => !existingTheme.games.some(existingGame => existingGame.id === newGame.id))
+
+      if (gamesToAdd.length > 0) {
+        await collection.updateOne(
+          { name: theme },
+          { $push: { games: { $each: gamesToAdd } } }
+        )
+      }
+
+      res.status(200).json({ message: 'Data updated successfully', addedGames: gamesToAdd.length })
+    } else {
+      const games = await Promise.all(ids.map(async (id) => {
+        const response = await fetch(`https://www.giantbomb.com/api/game/${id}/?api_key=${apiKey}&format=json`)
+        const data = await response.json()
+        return data
+      }))
+
+      const document = {
+        name: theme,
+        games: games
+      }
+
+      await collection.insertOne(document)
+
+      res.status(200).json({ message: 'Data inserted successfully' })
     }
-
-    await collection.insertOne(document)
-
-    res.status(200).json({ message: 'Data inserted successfully' })
   } catch (error) {
     console.error(error)
     res.status(500).json({ error: 'Failed to fetch and insert data' })
