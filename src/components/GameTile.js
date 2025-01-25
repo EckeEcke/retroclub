234f3dd7852b87b8c0e8697d6d7c3b457358b525
Christian Eckardt <eckardt.christian@web.de>
2025-01-25 14:39:27 +0100

    styling table

--- src/components/GameTile.js
+++ src/components/GameTile.js
@@ -76,32 +76,34 @@ const GameTile = ({ game }) => {
               <button className="rating-button" onClick={openModal}>Bewerten</button>
             )}
             <h3>Bewertungen</h3>
-            <table>
-              <thead>
-                <tr>
-                  <th>Player</th>
-                  <th>Gesamt</th>
-                  <th>Thema</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Lena</td>
-                  <td>{game.ratings && game.ratings.Lena ? game.ratings.Lena.total : '?'}</td>
-                  <td>{game.ratings && game.ratings.Lena ? game.ratings.Lena.theme : '?'}</td>
-                </tr>
-                <tr>
-                  <td>Rene</td>
-                  <td>{game.ratings && game.ratings.Rene ? game.ratings.Rene.total : '?'}</td>
-                  <td>{game.ratings && game.ratings.Rene ? game.ratings.Rene.theme : '?'}</td>
-                </tr>
-                <tr>
-                  <td>Chris</td>
-                  <td>{game.ratings && game.ratings.Chris ? game.ratings.Chris.total : '?'}</td>
-                  <td>{game.ratings && game.ratings.Chris ? game.ratings.Chris.theme : '?'}</td>
-                </tr>
-              </tbody>
-            </table>
+            <div class="table-wrapper">
+              <table>
+                <thead>
+                  <tr>
+                    <th>Player</th>
+                    <th>Gesamt</th>
+                    <th>Thema</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Lena</td>
+                    <td>{game.ratings && game.ratings.Lena ? game.ratings.Lena.total : '?'}</td>
+                    <td>{game.ratings && game.ratings.Lena ? game.ratings.Lena.theme : '?'}</td>
+                  </tr>
+                  <tr>
+                    <td>Rene</td>
+                    <td>{game.ratings && game.ratings.Rene ? game.ratings.Rene.total : '?'}</td>
+                    <td>{game.ratings && game.ratings.Rene ? game.ratings.Rene.theme : '?'}</td>
+                  </tr>
+                  <tr>
+                    <td>Chris</td>
+                    <td>{game.ratings && game.ratings.Chris ? game.ratings.Chris.total : '?'}</td>
+                    <td>{game.ratings && game.ratings.Chris ? game.ratings.Chris.theme : '?'}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
           {modalGame && (
             <RatingModal
