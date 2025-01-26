2331e5a60850aa5cc697480f7ab10cab8ff2c07a
Christian Eckardt <eckardt.christian@web.de>
2025-01-26 22:27:01 +0100

    stylings and delete form

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -8,8 +8,9 @@ function AdminPage() {
     const name = useThemeStore((state) => state.name)
     const [theme, setTheme] = useState('')
     const [gameIds, setGameIds] = useState('')
+    const [deleteTheme, setDeleteTheme] = useState('')
 
-    const handleSubmit = async (e) => {
+    const handleAddSubmit = async (e) => {
         e.preventDefault()
         const gameIdsArray = gameIds.split(',').map(id => id.trim())
         const response = await fetch('/api/addgames', {
@@ -26,34 +27,67 @@ function AdminPage() {
         }
     }
 
+    const handleDeleteSubmit = async (e) => {
+        e.preventDefault()
+        const response = await fetch('https://retroclub.vercel.app/api/deleteTheme', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ theme: deleteTheme, key })
+        })
+        if (response.ok) {
+            console.log('Theme deleted successfully')
+        } else {
+            console.error('Failed to delete theme')
+        }
+    }
+
     if (!key || !name) {
         return <p>Access Denied</p>
     }
 
     return (
         <div className="container admin-page">
-            <h2>Neues Thema mit Spielen hinzufügen</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="theme">Thema:</label>
-                    <input
-                        type="text"
-                        id="theme"
-                        value={theme}
-                        onChange={(e) => setTheme(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="gameIds">Game IDs:</label>
-                    <input
-                        type="text"
-                        id="gameIds"
-                        value={gameIds}
-                        onChange={(e) => setGameIds(e.target.value)}
-                    />
-                </div>
-                <button type="submit">Absenden</button>
-            </form>
+            <div>
+                <h2>Neues Thema mit Spielen hinzufügen</h2>
+                <form onSubmit={handleAddSubmit}>
+                    <div>
+                        <label htmlFor="theme">Thema:</label>
+                        <input
+                            type="text"
+                            id="theme"
+                            value={theme}
+                            onChange={(e) => setTheme(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="gameIds">Game IDs:</label>
+                        <input
+                            type="text"
+                            id="gameIds"
+                            value={gameIds}
+                            onChange={(e) => setGameIds(e.target.value)}
+                        />
+                    </div>
+                    <button type="submit">Absenden</button>
+                </form>
+            </div>
+            <div>
+                <h2>Thema löschen</h2>
+                <form onSubmit={handleDeleteSubmit}>
+                    <div>
+                        <label htmlFor="deleteTheme">Thema:</label>
+                        <input
+                            type="text"
+                            id="deleteTheme"
+                            value={deleteTheme}
+                            onChange={(e) => setDeleteTheme(e.target.value)}
+                        />
+                    </div>
+                    <button type="submit">Löschen</button>
+                </form>
+            </div>  
         </div>
     )
 }
