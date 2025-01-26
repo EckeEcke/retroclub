832731d005f72c41b123d746083066bf851b319c
Christian Eckardt <eckardt.christian@web.de>
2025-01-26 17:11:48 +0100

    add touch controls to image gallery

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -1,20 +1,50 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import '../css/ImageGallery.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const ImageGallery = ({ images, onClose }) => {
   const [currentIndex, setCurrentIndex] = useState(0)
+  const touchStartX = useRef(0)
+  const touchEndX = useRef(0)
+
+  const nextImage = useCallback(() => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length)
+  }, [images.length])
+
+  const prevImage = useCallback(() => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length)
+  }, [images.length])
 
   useEffect(() => {
-  }, [images])
+    const handleTouchStart = (event) => {
+      touchStartX.current = event.touches[0].clientX
+    }
 
-  const nextImage = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length)
-  }
+    const handleTouchMove = (event) => {
+      touchEndX.current = event.touches[0].clientX
+    }
 
-  const prevImage = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length)
-  }
+    const handleTouchEnd = () => {
+      if (touchStartX.current - touchEndX.current > 50) {
+        nextImage()
+      }
+
+      if (touchStartX.current - touchEndX.current < -50) {
+        prevImage()
+      }
+    }
+
+    const galleryElement = document.querySelector('.image-gallery')
+    galleryElement.addEventListener('touchstart', handleTouchStart)
+    galleryElement.addEventListener('touchmove', handleTouchMove)
+    galleryElement.addEventListener('touchend', handleTouchEnd)
+
+    return () => {
+      galleryElement.removeEventListener('touchstart', handleTouchStart)
+      galleryElement.removeEventListener('touchmove', handleTouchMove)
+      galleryElement.removeEventListener('touchend', handleTouchEnd)
+    }
+  }, [nextImage, prevImage])
 
   const handleError = () => {
     nextImage()
