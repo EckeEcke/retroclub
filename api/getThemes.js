d33f309710c034144bd251d2e687d53b25f107f6
Christian Eckardt <eckardt.christian@web.de>
2025-02-10 19:01:47 +0100

    cleanup

--- api/getThemes.js
+++ api/getThemes.js
@@ -43,28 +43,28 @@ export default async function handler(req, res) {
             theme.games.forEach((game) => {
               if (game.ratings) {
                 if (game.ratings.Christian) {
-                  game.ratings.Christian.gameplay = game.ratings.Christian.total
+                  game.ratings.Christian.gameplay = game.ratings.Christian.total || game.ratings.Christian.gameplay
                   delete game.ratings.Christian.total
-                  game.ratings.Christian.aging = game.ratings.Christian.theme
+                  game.ratings.Christian.aging = game.ratings.Christian.theme || game.ratings.Christian.aging
                   delete game.ratings.Christian.theme
-                  game.ratings.Christian.graphics = null
-                  game.ratings.Christian.trashiness = null
+                  game.ratings.Christian.graphics = game.ratings.Christian.graphics ?? null
+                  game.ratings.Christian.trashiness = game.ratings.Christian.trashiness ?? null
                 }
                 if (game.ratings.Rene) {
-                  game.ratings.Rene.gameplay = game.ratings.Rene.total
+                  game.ratings.Rene.gameplay = game.ratings.Rene.total || game.ratings.Rene.gameplay
                   delete game.ratings.Rene.total
-                  game.ratings.Rene.aging = game.ratings.Rene.theme
+                  game.ratings.Rene.aging = game.ratings.Rene.theme || game.ratings.Rene.aging
                   delete game.ratings.Rene.theme
-                  game.ratings.Rene.graphics = null
-                  game.ratings.Rene.trashiness = null
+                  game.ratings.Rene.graphics = game.ratings.Rene.graphics ?? null
+                  game.ratings.Rene.trashiness = game.ratings.Rene.trashiness ?? null
                 }
                 if (game.ratings.Lena) {
-                  game.ratings.Lena.gameplay = game.ratings.Lena.total
+                  game.ratings.Lena.gameplay = game.ratings.Lena.total || game.ratings.Lena.gameplay
                   delete game.ratings.Lena.total
-                  game.ratings.Lena.aging = game.ratings.Lena.theme
+                  game.ratings.Lena.aging = game.ratings.Lena.theme || game.ratings.Lena.aging
                   delete game.ratings.Lena.theme
-                  game.ratings.Lena.graphics = null
-                  game.ratings.Lena.trashiness = null
+                  game.ratings.Lena.graphics = game.ratings.Lena.graphics ?? null
+                  game.ratings.Lena.trashiness = game.ratings.Lena.trashiness ?? null
                 }
               }
             })
@@ -72,9 +72,14 @@ export default async function handler(req, res) {
         })
       }
 
+      const updateFields = { games: document.games }
+      if (document.themes === null) {
+        updateFields.themes = undefined
+      }
+
       await collection.updateOne(
         { _id: document._id },
-        { $set: { themes: document.themes } }
+        { $set: updateFields }
       )
     })
     //
